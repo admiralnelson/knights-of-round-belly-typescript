1014a5b16c2f5b9add3288cb49fdffc77bba2d9e
admiralnelson <admiralofinternetmeme@outlook.com>
2023-01-09 23:55:31 +0700

    added more logging features

--- campaign/mod/_KnightsOfRoundBellyLogger.ts
+++ campaign/mod/_KnightsOfRoundBellyLogger.ts
@@ -5,20 +5,50 @@ namespace AdmiralNelsonKnightsOfTheRoundBelly {
     
         constructor(name: string) { this._name = name }
     
-        public Log(s: string): void { out(`${this._name}: ${s}`) }
-    
-        public LogWarn(s: string): void
-        {
-            if(PrintWarning != null) PrintWarning(`${this._name}: ${s}\n`); else out(`${this._name} WARNING ${s}`)
+        /**
+         * log into output stream
+         * @param s message
+         * @param showName prints the name associated to this log (defaults true)
+         */
+        public Log(s: string, showName: boolean = true): void {
+             showName ? out(`${this._name}: ${s}`) : out(s) 
         }
     
-        public LogError(s: string): void
+        /**
+         * log into output stream with WARNING prefix,  
+         * if the game is launched from SNED Loader, it will be highlighted yellow
+         * @param s message
+         * @param showName prints the name associated to this log (defaults true)
+         */
+        public LogWarn(s: string, showName: boolean = true): void
+        {
+            if(PrintWarning != null) {
+                (showName) ? PrintWarning(`${this._name}: ${s}\n`) : PrintWarning(`${s}\n`)
+            } else {
+                (showName) ? out(`${this._name} WARNING :${s}`) : out(`WARNING: ${s}`)
+            } 
+        }
+    
+         /**
+         * log into output stream with ERROR prefix,  
+         * if the game is launched from SNED Loader, it will be highlighted red
+         * @param s message
+         * @param showTraceback prints the trackback up to this error statement (defaults true)
+         * @param showName prints the name associated to this log (defaults true)
+         */
+        public LogError(s: string, showTraceback: boolean = true, showName: boolean = true): void
         {
             const traceback = debug.traceback("", 2)
-            PrintError != null ? PrintError(`${this._name}: ${s} \n`) : out(`${this._name} ERROR ${s}`)
-            this.LogWarn("================\n")
-            this.LogWarn(`${traceback} \n`)
-            this.LogWarn("================\n")
+            if(PrintError != null) {
+                (showName) ? PrintError(`${this._name}: ${s}\n`) : PrintError(`${s}\n`)
+            } else {
+                (showName) ? out(`${this._name} ERROR: ${s}`) : out(`ERROR: ${s}`)
+            }
+            if(showTraceback) {
+                this.LogWarn("================\n")
+                this.LogWarn(`${traceback} \n`)
+                this.LogWarn("================\n")
+            }            
         }    
     }
 }
